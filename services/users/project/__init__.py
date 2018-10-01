5614e5323e963a955733ee5be92963f6c7769c2e
Jeff Lindholm <jeff@lindholm.org>
2018-10-01 15:44:29 -0400

    react app, mostly working

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,19 +2,31 @@ import os
 from flask import Flask
 from flasgger import Swagger
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(script_info=None):
     app = Flask(__name__)
-    Swagger(app)
+    CORS(app)
+    swagger_config = Swagger.DEFAULT_CONFIG
+    swagger_config['swagger_ui_bundle_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js'
+    swagger_config['swagger_ui_standalone_preset_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-standalone-preset.js'
+    swagger_config['jquery_js'] = '//unpkg.com/jquery@2.2.4/dist/jquery.min.js'
+    swagger_config['swagger_ui_css'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui.css'
+    # swagger_config['base_dir'] = '/'
+    Swagger(app, config=swagger_config)
+
 
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
 
     db.init_app(app)
+    toolbar.init_app(app)
 
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
